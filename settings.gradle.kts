1b05833ab26fc31fbede2123466725b3a3f9250e
Himanshu Mall <62309305+Hitman7084@users.noreply.github.com>
2025-08-10 01:33:20 +0530

    Add Maven Central repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,10 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
-        mavenCentral()
+        mavenCentral() // official Supabase libraries
         maven { url = uri("https://jitpack.io") }
     }
 }
